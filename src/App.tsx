14966b5a80dd297932f8bda493bfc8ccce701d22
Oleh Musii <oleg.musiy321@gmail.com>
2026-05-12 11:59:42 +0300

    react movies project

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+import SearchBar from './components/SearchBar/SearchBar';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import MovieGrid from './components/MovieGrid/MovieGrid';
+import MovieModal from './components/MovieModal/MovieModal';
+
+import { fetchMovies } from './services/movieService';
+
+import type { Movie } from './types/movie';
+
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]);
+      setError(false);
+      setLoading(true);
+
+      const data = await fetchMovies(query);
+
+      if (data.length === 0) {
+        toast.error('No movies found for your request.');
+      }
+
+      setMovies(data);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Toaster position="top-right" />
+
+      <SearchBar onSubmit={handleSearch} />
+
+      {loading && <Loader />}
+
+      {error && <ErrorMessage />}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+
+      {movies.length > 0 && (
+        <MovieGrid
+          movies={movies}
+          onSelect={setSelectedMovie}
+        />
+      )}
+    </>
+  );
+}
---
+++ src/services/movieService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import type { Movie } from '../../types/movie';
+
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const token = import.meta.env.VITE_TMDB_TOKEN;
+
+export const fetchMovies = async (
+  query: string
+): Promise<Movie[]> => {
+  const response = await axios.get<FetchMoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        query,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data.results;
+};
